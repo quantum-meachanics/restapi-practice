0a3718063d7a13b5e7e694ab247ba561a40b6010
yanghyeyeon <yhy4058@gmail.com>
2024-09-04 11:48:23 +0900

    service에 어노테이션 추가

--- restapi practice/src/main/java/com/ohgiraffers/restapipractice/service/PostService.java
+++ restapi practice/src/main/java/com/ohgiraffers/restapipractice/service/PostService.java
@@ -1,7 +1,9 @@
 package com.ohgiraffers.restapipractice.service;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class PostService {
 }
