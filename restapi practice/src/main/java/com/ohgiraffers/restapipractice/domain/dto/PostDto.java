62a61e62f33f1abbed77b60715bb72d678952ef0
yanghyeyeon <yhy4058@gmail.com>
2024-09-04 11:00:02 +0900

    [feature]entity,dto 생성

--- restapi practice/src/main/java/com/ohgiraffers/restapipractice/domain/dto/PostDto.java
+++ restapi practice/src/main/java/com/ohgiraffers/restapipractice/domain/dto/PostDto.java
@@ -1,4 +1,15 @@
 package com.ohgiraffers.restapipractice.domain.dto;
 
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
 public class PostDto {
+
+    private int id;
+    private String title;
+    private String content;
 }
--- restapi practice/src/main/java/com/ohgiraffers/restapipractice/domain/entity/Post.java
+++ restapi practice/src/main/java/com/ohgiraffers/restapipractice/domain/entity/Post.java
@@ -1,4 +1,22 @@
 package com.ohgiraffers.restapipractice.domain.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "tbl_board")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Builder
 public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String title;
+    private String content;
 }
