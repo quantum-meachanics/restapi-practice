1dc76c913fc938b777a71c217b112ef08da26929
yanghyeyeon <yhy4058@gmail.com>
2024-09-04 12:59:30 +0900

    게시글 추가 기능 생성

--- restapi practice/src/main/java/com/ohgiraffers/restapipractice/service/PostService.java
+++ restapi practice/src/main/java/com/ohgiraffers/restapipractice/service/PostService.java
@@ -1,9 +1,26 @@
 package com.ohgiraffers.restapipractice.service;
 
+import com.ohgiraffers.restapipractice.domain.dto.PostDto;
+import com.ohgiraffers.restapipractice.domain.entity.Post;
+import com.ohgiraffers.restapipractice.repository.PostRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class PostService {
+
+    private final PostRepository postRepository;
+
+
+    public PostDto createPost(PostDto newPost) {
+
+        Post post = Post.builder()
+                .title(newPost.getTitle())
+                .content(newPost.getContent()).build();
+
+        postRepository.save(post);
+
+        return newPost;
+    }
 }
