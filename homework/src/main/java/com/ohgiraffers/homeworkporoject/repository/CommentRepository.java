72cfda09a79154d3282276066ea80b6c7e92fbb3
parkhayarn <s0224y92@gmail.com>
2024-09-09 10:51:28 +0900

    delete

--- homework/src/main/java/com/ohgiraffers/homeworkporoject/repository/CommentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ohgiraffers.homeworkporoject.repository;
-
-import com.ohgiraffers.homeworkporoject.domain.entity.Comment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CommentRepository extends JpaRepository<Comment, Long> {
-}
